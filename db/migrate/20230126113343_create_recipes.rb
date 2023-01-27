6be631fd180ecdc1daafc69c55901265b382fd21
root <root@DESKTOP-09EMUN4.localdomain>
2023-01-27 20:11:57 +0800

    created tables

---
+++ db/migrate/20230126113343_create_recipes.rb
@@ -0,0 +1,14 @@
+class CreateRecipes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipes do |t|
+      t.string :dish_name
+      t.text :ingredient
+      t.text :direction
+      t.text :suggestion
+      t.integer :comments_counter, default: 0
+      t.integer :likes_counter, default: 0
+      
+      t.timestamps
+    end
+  end
+end
