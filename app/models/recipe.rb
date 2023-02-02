54c63c1064d2bae64a73099d285476f6ecc18e2c
root <root@DESKTOP-09EMUN4.localdomain>
2023-02-02 23:11:51 +0800

    added validations to models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,11 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  validates :dish_name, presence: true,
+                        length: { minimum: 5 }
+  validates :ingredient, presence: true,
+                         length: { minimum: 10 }
+  validates :direction, presence: true,
+                        length: { minimum: 10 }
 end
