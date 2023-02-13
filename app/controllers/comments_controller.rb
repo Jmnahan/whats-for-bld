5602a44eef78879ce7cbbd4a1f038d1dd1f7acb0
root <root@DESKTOP-09EMUN4.localdomain>
2023-02-13 20:06:14 +0800

    admin comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,10 @@ class CommentsController < ApplicationController
   before_action :get_recipe
 
   def create
-    @comment = @recipe.comments.build(text: comment_params[:text], user_id: current_user.id, recipe_id: @recipe.id)
-    if @comment.save
-      redirect_to recipe_path(@recipe) 
-    else
-      render :new
-    end
+    @comment = @recipe.comments.new(comment_params)
+    @comment.user = current_user
+    @comment.save 
+    redirect_to recipe_path(@recipe) 
   end
 
   def destroy
