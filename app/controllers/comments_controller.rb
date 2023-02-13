23fd745fe9e999d550f77b5029bb14c2e10bfd9a
root <root@DESKTOP-09EMUN4.localdomain>
2023-02-13 15:27:58 +0800

    implemented delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,6 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :get_recipe
 
-  def index
-    @comments = @recipe.comments
-  end
-
-  def new
-    @comment = @recipe.comments.build
-  end
-
   def create
     @comment = @recipe.comments.build(text: comment_params[:text], user_id: current_user.id, recipe_id: @recipe.id)
     
@@ -20,28 +12,23 @@ class CommentsController < ApplicationController
     end
   end
 
-  def show
-    @comment = @recipe.comments.find(params[:id])
-  end
+  # def edit
+  #   @comment = @recipe.comments.find(params[:id])
+  # end
 
-  def edit
-    @comment = @recipe.comments.find(params[:id])
-  end
-
-  def update
-    @comment = @recipe.comments.find(params[:id])
-    if @comment.update(comment_params)
-      redirect_to root_path
-    else 
-      render :edit, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   @comment = current_user.comments.find(params[:id])
+  #   if @comment.update(comment_params)
+  #     redirect_to recipe_path(@recipe) 
+  #   else 
+  #     render :edit, status: :unprocessable_entity
+  #   end
+  # end
 
   def destroy
-    @comment = @article.comments.find(params[:id])
-    @comment.destroy
-
-    redirect_to root_path
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy   
+    redirect_to recipe_path(@recipe) 
   end
 
   private
