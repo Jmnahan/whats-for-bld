efe1a78b8ba8421e09c5eb3b87f35d6c6e8383c0
root <root@DESKTOP-09EMUN4.localdomain>
2023-02-15 23:25:28 +0800

    recipe test

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:recipe) { create :recipe }
+
+  describe 'Dish name' do
+    it 'should be present' do
+      recipe = build :recipe, dish_name: nil
+      expect(recipe.save).to be false
+    end
+
+    it 'should be atleast 5 characters' do
+      recipe = build :recipe, dish_name: "dish"
+      expect(recipe.save).to be false
+    end
+  end
+
+  describe 'Ingredient' do
+    it 'should be present' do
+      recipe = build :recipe, ingredient: nil
+      expect(recipe.save).to be false
+    end
+
+    it 'should be atleast 10 characters' do
+      recipe = build :recipe, ingredient: "testing"
+      expect(recipe.save).to be false
+    end
+  end
+
+  describe 'Direction' do
+    it 'should be present' do
+      recipe = build :recipe, direction: nil
+      expect(recipe.save).to be false
+    end
+
+    it 'should be atleast 10 characters' do
+      recipe = build :recipe, direction: "testing"
+      expect(recipe.save).to be false
+    end
+  end
 end
