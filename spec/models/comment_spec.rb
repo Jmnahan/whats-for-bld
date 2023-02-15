f791c4301bbc6f553fc9094269697029dacf960b
root <root@DESKTOP-09EMUN4.localdomain>
2023-02-15 23:46:39 +0800

    comments test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:comment) { create :comment }
+
+  describe 'Comment' do
+    it 'should have text' do
+      comment = build :comment, text: nil
+      expect(comment.save).to be false
+    end
+  end
+
+  describe 'Comment counter' do
+    before do
+      @user1 = create(:user)
+      @recipe1 = create(:recipe)
+      comment = create(:comment, user: @user1, recipe: @recipe1)
+    end
+
+    it "Returns number of posts for a user" do
+      expect(@recipe1.comments.count).to eq(1)
+    end
+  end
 end
