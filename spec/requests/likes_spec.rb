e10a030e4f9c343643b6eedb5b6a4d247051428a
root <root@DESKTOP-09EMUN4.localdomain>
2023-02-17 19:20:17 +0800

    likes request test

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Likes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:like) { create :like }
+
+  before do
+    @user = create(:user)
+    @recipe = create(:recipe)
+    like = create(:like, user: @user, recipe: @recipe)
+  end
+
+  describe 'can be createad' do
+    it 'Returns number of likes' do
+      expect(@recipe.likes.count).to eq(1)
+    end
   end
 end
