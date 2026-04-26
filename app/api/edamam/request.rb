8fa4790a2302b0394feb30f6729402e968537e3f
Jhona Rica Manahan <manhan.jrd@gmail.com>
2026-04-27 02:08:03 +0800

    fix: deployment error/syntax

--- app/api/edamam/request.rb
+++ app/api/edamam/request.rb
@@ -1,13 +1,13 @@
 module Edamam
     class Request
         BASE_URL = 'https://api.edamam.com/api/nutrition-data'
-        app_id: ENV['EDAMAM_APP_ID'],
-        app_key: ENV['EDAMAM_APP_KEY']
+        APP_ID  =  ENV['EDAMAM_APP_ID'],
+        APP_KEY = ENV['EDAMAM_APP_KEY']
 
         def self.call(http_method, endpoint)
             result = RestClient::Request.execute(
                 method: http_method,
-                url: "#{BASE_URL}?#{app_id}&#{app_key}#{endpoint}",
+                url: "#{BASE_URL}?#{APP_ID}&#{APP_KEY}#{endpoint}",
                 headers: {"content-type" => "application/json;charset=UTF-8"}
             )
         end
